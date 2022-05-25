caa4384b0cb22b2aac9503bf97eb68fa2e108bfb
Teslim <teslimjimoh191@gmail.com>
2022-05-25 11:26:51 +0100

    created procfile

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '45a2294a77702b8f81691157b7981e48a2e6e893',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '45a2294a77702b8f81691157b7981e48a2e6e893',
+            'dev_requirement' => false,
+        ),
+    ),
+);
